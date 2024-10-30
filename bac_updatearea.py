268547649d09eb5cbcca7a8962cbde97b8e4298b
jacknguyen2025 <jacknguyen2025@gmail.com>
2024-10-30 14:34:27 +0700

    Update bac_updatearea.py

--- bac_updatearea.py
+++ bac_updatearea.py
@@ -1,7 +1,7 @@
 from PyQt5.QtGui import QIcon
 import os
 from PyQt5 import uic
-from PyQt5.QtWidgets import QAction, QDialog, QMessageBox, QProgressDialog, QApplication
+from PyQt5.QtWidgets import QAction, QDialog, QMessageBox, QApplication
 from qgis.PyQt.QtCore import QVariant, Qt
 from qgis.core import QgsField
 from qgis.utils import iface
@@ -12,54 +12,84 @@ class AreaUpdateDialog(QDialog):
         ui_path = os.path.join(os.path.dirname(__file__), 'AreaUpdateDialog.ui')
         uic.loadUi(ui_path, self)
         
-        # Kết nối nút OK và nút "Mặc định"
-        self.buttonBox.accepted.connect(self.accept)
+        # Cờ hủy tiến trình
+        self.canceled = False
+        
+        # Kết nối các nút với các phương thức
+        self.btnUpdate.clicked.connect(self.start_update)
+        self.btnStopSave.clicked.connect(self.save_and_close)
+        self.btnClose.clicked.connect(self.close_dialog)
         self.btnDefault.clicked.connect(self.set_default_values)
 
+
     def set_default_values(self):
-        # Gán giá trị mặc định cho các ô nhập diện tích
         self.lineEdit_area_sq.setText("area_sq")
         self.lineEdit_area_ha.setText("area_ha")
 
     def get_field_names(self):
-        # Lấy tên trường diện tích và trạng thái checkbox
         area_sq_field = self.lineEdit_area_sq.text()
         area_ha_field = self.lineEdit_area_ha.text()
         selected_features = self.checkBox_selected_features.isChecked()
         return area_sq_field, area_ha_field, selected_features
 
-class ProgressDialog(QDialog):
-    def __init__(self, total_steps, parent=None):
-        super(ProgressDialog, self).__init__(parent)
-        
-        # Tải file giao diện cho hộp thoại tiến độ
-        ui_path = os.path.join(os.path.dirname(__file__), 'ProgressDialog.ui')
-        uic.loadUi(ui_path, self)
-
-        # Thiết lập hộp thoại tiến độ
-        self.setWindowTitle("Processing...")
-        self.setWindowModality(Qt.WindowModal)
-        
-        # Thiết lập giá trị ban đầu cho progress bar và cờ hủy
-        self.progressBar.setMinimum(0)
-        self.progressBar.setMaximum(total_steps)
+    def start_update(self):
+        # Đặt lại cờ hủy
         self.canceled = False
+
+        # Lấy thông tin trường từ người dùng
+        area_sq_field, area_ha_field, selected_features_only = self.get_field_names()
         
-        # Kết nối nút Cancel
-        self.btnCancel.clicked.connect(self.mark_canceled)
+        # Lấy layer hiện tại
+        layer = iface.activeLayer()
+        if not layer.isEditable():
+            layer.startEditing()
+        
+        # Thêm các trường diện tích nếu chưa có
+        if area_sq_field not in [field.name() for field in layer.fields()]:
+            layer.dataProvider().addAttributes([QgsField(area_sq_field, QVariant.Double, "double", 10, 2)])
+            layer.updateFields()
 
-    def mark_canceled(self):
-        # Đánh dấu tiến trình là đã hủy
+        if area_ha_field not in [field.name() for field in layer.fields()]:
+            layer.dataProvider().addAttributes([QgsField(area_ha_field, QVariant.Double, "double", 10, 2)])
+            layer.updateFields()
+
+        # Chuyển đổi các đối tượng đã chọn thành danh sách và thiết lập tiến trình
+        selected_features = list(layer.selectedFeatures()) if selected_features_only and layer.selectedFeatureCount() > 0 else list(layer.getFeatures())
+        total_features = len(selected_features)
+        self.progressBar.setMaximum(total_features)
+        
+        count = 0
+        for i, feature in enumerate(selected_features):
+            # Kiểm tra nếu tiến trình bị hủy
+            if self.canceled:
+                QMessageBox.information(self, "Canceled", "Update was canceled.")
+                break
+
+            # Cập nhật diện tích
+            feature[area_sq_field] = feature.geometry().area()
+            feature[area_ha_field] = feature.geometry().area() / 10000
+            layer.updateFeature(feature)
+            count += 1
+
+            # Cập nhật thanh tiến trình
+            self.progressBar.setValue(i + 1)
+            QApplication.processEvents()  # Cập nhật giao diện ngay lập tức
+
+        # Thông báo hoàn tất
+        if not self.canceled:
+            layer.commitChanges()
+            QMessageBox.information(self, "Completed", f"{count} features updated.")
+        else:
+            layer.rollBack()  # Hủy các thay đổi nếu bị hủy
+
+    def save_and_close(self):
+        # Đặt cờ hủy và đóng hộp thoại
         self.canceled = True
+        self.close()
 
-    def is_canceled(self):
-        # Kiểm tra xem người dùng có hủy tiến trình không
-        return self.canceled
-
-    def update_progress(self, step):
-        # Cập nhật thanh tiến độ
-        self.progressBar.setValue(step)
-        QApplication.processEvents()  # Cập nhật giao diện người dùng ngay lập tức
+    def close_dialog(self):
+        # Đóng hộp thoại mà không lưu
+        self.close()
 
 class AreaUpdatePlugin:
     def __init__(self, iface):
@@ -67,26 +97,17 @@ class AreaUpdatePlugin:
         self.dialog = None
 
     def initGui(self):
-         print("Initializing GUI")
-        # Đường dẫn đến icon của plugin
         icon_path = os.path.join(os.path.dirname(__file__), "icon.png")
         self.action = QAction(QIcon(icon_path), "Update Area Fields", self.iface.mainWindow())
-        
-        # Kết nối action với phương thức run
         self.action.triggered.connect(self.run)
-        
-        # Thêm plugin vào toolbar và menu
         self.iface.addToolBarIcon(self.action)
-        self.iface.addPluginToMenu("Bac Update Area", self.action)
+        self.iface.addPluginToMenu("Bac Area Update Plugin", self.action)
 
     def unload(self):
-        # Xóa plugin khỏi toolbar và menu
-        self.iface.removePluginMenu("Bac Update Area", self.action)
+        self.iface.removePluginMenu("Bac Area Update Plugin", self.action)
         self.iface.removeToolBarIcon(self.action)
 
     def run(self):
-   print("Plugin activated")
-   if not self.dialog:
+        if not self.dialog:
             self.dialog = AreaUpdateDialog()
-        
- 
+        self.dialog.show()
